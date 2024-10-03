bea22b04a782237f9dc0435fd7426b7a5c27a5f3
Nirmal W <akalankawijesing@gmail.com>
2024-10-04 03:41:52 +0530

    updated v.1

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,13 @@ import {
   Route,
   Routes,
 } from "react-router-dom";
-
+import Header from "./components/Header";
 
 export default function App(props) {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Index />} />
-        <Route exact path="/:dinosaur" element={<Dinosaur />} />
+        <Route exact path="/" element={<Header/>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
